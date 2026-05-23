622527b90108678326847566f279e110c0c6ede7
GravelLi <148532924+GravelLi@users.noreply.github.com>
2026-05-23 18:54:12 +0800

    Update LocationComparator.java

--- src/LocationComparator.java
+++ src/LocationComparator.java
@@ -1,9 +1,11 @@
 public class LocationComparator {
     public static int compare(Location a, Location b) {
+        // Higher priority score should come first.
         if (a.getPriorityScore() != b.getPriorityScore()) {
             return b.getPriorityScore() - a.getPriorityScore();
         }
 
+        // If scores are the same, sort by location ID alphabetically.
         return a.getLocationId().compareTo(b.getLocationId());
     }
 }
