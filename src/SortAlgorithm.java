ca43eeaf240771cdc54fcc303a05562889ff4d85
GravelLi <148532924+GravelLi@users.noreply.github.com>
2026-05-23 18:57:55 +0800

    Update SortAlgorithm.java

--- src/SortAlgorithm.java
+++ src/SortAlgorithm.java
@@ -1,5 +1,7 @@
 public interface SortAlgorithm {
+    // All sorting algorithms should provide the same sort method.
     void sort(Location[] locations);
 
+    // This name is used when printing the algorithm result.
     String getName();
 }
