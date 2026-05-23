424b2c5a632f1e2bc811480aa53b53d61b035b9a
GravelLi <148532924+GravelLi@users.noreply.github.com>
2026-05-23 18:52:39 +0800

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -3,6 +3,7 @@ public class Edge {
     private int weight;
 
     public Edge(String toLocation, int weight) {
+        // Store the target node and the cost of travelling to it.
         this.toLocation = toLocation;
         this.weight = weight;
     }
